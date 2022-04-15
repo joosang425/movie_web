af5bb3213375b665cdfa4b350e459d875a5617b6
JooSang <68217970+joosang425@users.noreply.github.com>
2022-04-15 16:03:30 +0900

    #3

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,7 @@ import React from "react";
 import ReactDOM  from "react-dom";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-
-// 모드 변경 기능 추가 시 변수명이 같아야 함
-
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-}
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-}
+import { darkTheme, lightTheme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
